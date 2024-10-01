1b29a5540a516dd537a5c52a77b7855b228a7c6e
GustavoRamosS3 <Guhpramos@gmail.com>
2024-10-01 10:37:00 -0300

    first commit

---
+++ prisma/migrations/20241001122908_add_data_2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Usuario" ALTER COLUMN "data_nasc" DROP DEFAULT;
---
+++ src/routes/usuario.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as usuario from '../controllers/usuario';
+
+const rota = Router();
+rota.get('/usuario', usuario.getUsuarios);
+rota.get('/usuario/:id', usuario.buscarPorId);
+
+export default rota;
